78aa0c49f6c52839c346bd83c78e63da4b176bd8
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-07-17 13:28:52 -0300

    FEAT: Stock Model

---
+++ Models/stock_model.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Blueprint, request, jsonify
+from Extras import db
+import werkzeug.security
+
+class Watchlist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    stock_symbol = db.Column(db.String(10), nullable=False)  # "AAPL", "PETR4"
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    user = db.relationship('User', backref=db.backref('watchlist', lazy=True))
